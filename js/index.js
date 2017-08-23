15ca1ee6efe92f16d64a0aeb0418fdc1570cf411
pjliddy <pjliddy@gmail.com>
2017-08-23 16:33:43 -0400

    Updated rough layout to match wireframes for home
    Simplified index.html. Elements get added through javascript when page
    is loaded. Applied styles to match wireframe layout. Maxed width at
    1200px.

--- js/index.js
+++ js/index.js
@@ -1,48 +1,91 @@
 'use strict';
 
+// global array to store story data objects from JSON
 let storyData = [];
 
-// JavaScript Functions
+// document ready
 $(function(){
   getData('exercise-sample-data.json');
+  handleEvents();
 });
 
+// event handlers
+function handleEvents() {
+  $(document).on('click', '.title a', showArticle);
+};
+
+// override default link behavior and show article at event.target.href
+function showArticle(event) {
+  event.preventDefault();
+  console.log(event.target.href);
+};
+
+// make jQuery AJAX call to get json data file
 function getData (url) {
   $.ajax({
     dataType: "json",
     url: url
   })
+  // handle successful promise result
   .done(function(data) {
     // save data is global array of stories
     storyData = data.response.docs
     renderHome();
   })
+  // handle unsuccessful promise result
   .fail(function(err) {
     console.log(err);
   });
 };
 
 function renderHome () {
-  // console.log(storyData);
-
-  // render primary article
-  renderPrimaryFeature(storyData[0]);
-
-  // render two secondary articles
-  renderSecondaryFeatures(storyData.slice(1,2));
-
-
+  // render primary featured article
+  if (storyData.length > 0) {
+    renderPrimaryFeature(storyData[0]);
+  }
+  // render secondary featured articles
+  if (storyData.length > 2) {
+    renderSecondaryFeatures(storyData.slice(1,3));
+  }
   // render list of remaining articles
-  renderArticleList(storyData.slice(3));
+  if (storyData.length > 3) {
+    renderArticleList(storyData.slice(3));
+  }
 };
 
 // render primary feature on home page
 function renderPrimaryFeature(articleData) {
   $('.feature-primary').append('<div class="hero"></div>');
-  $('.feature-primary .hero').append('<h2 class="article-title"><a href="' + articleData.web_url + '" target="_blank">' + articleData.headline.main + '</a></h2>');
-  $('.feature-primary .hero').append('<p class="byline"><span class="author">' + articleData.byline.original + '</span><span class="timestamp">' + formatTimestamp(articleData.pub_date) + '</span></p>');
-  $('.feature-primary .hero').append('<img class="thumbnail" src="' + getThumbnail(articleData) + '"/>');
-  $('.feature-primary').append('<p class="lead-para">' + articleData.lead_paragraph + '</p>');
+
+  $('.feature-primary .hero').append(
+    '<img class="thumbnail" src="' +
+    getThumbnail(articleData) +
+    '"/>'
+  );
+
+  $('.feature-primary .hero').append(
+    '<h2 class="title"><a href="' +
+    articleData.web_url +
+    '" target="_blank">' +
+    articleData.headline.main +
+    '</a></h2>'
+  );
+
+  $('.feature-primary .hero').append(
+    '<p class="byline"><span class="author">' +
+    articleData.byline.original.toLowerCase() +
+    '</span> <span class="timestamp">' +
+    formatTimestamp(articleData.pub_date) +
+    '</span></p>'
+  );
+
+  $('.feature-primary .hero').append('<div class="clearfix"></div>');
+
+  $('.feature-primary').append(
+    '<p class="lead-para">' +
+    articleData.lead_paragraph +
+    '</p>'
+  );
 };
 
 // render series of secondary features on home page
@@ -52,11 +95,28 @@ function renderSecondaryFeatures(articles) {
 
 // render secondary feature
 function renderSecondaryFeature(articleData, i) {
+  // create secondary feature div
   $('.features-secondary').append('<div class="feature-secondary"></div>');
-  const featureDiv = $('.features-secondary .feature-secondary').get(i);
 
-  $(featureDiv).append('<h2 class="article-title"><a href="' + articleData.web_url + '" target="_blank">' + articleData.headline.main + '</a></h2>');
-  $(featureDiv).append('<p class="byline"><span class="author">' + articleData.byline.original + '</span><span class="timestamp">' + formatTimestamp(articleData.pub_date) + '</span></p>');
+  // define current secondary feature div in features-secondary
+  const articleDiv = $('.features-secondary .feature-secondary').get(i);
+
+  // add HTML elements for feature
+  $(articleDiv).append(
+    '<h2 class="title"><a href="' +
+    articleData.web_url +
+    '" target="_blank">' +
+    articleData.headline.main +
+    '</a></h2>'
+  );
+
+  $(articleDiv).append(
+    '<p class="byline"><span class="author">' +
+    articleData.byline.original.toLowerCase() +
+    '</span><br /><span class="timestamp">' +
+    formatTimestamp(articleData.pub_date) +
+    '</span></p>'
+  );
 };
 
 // generate list of non-featured articles
@@ -69,19 +129,57 @@ function renderArticle(articleData, i) {
   // create article div
   $('.article-list').append('<div class="article"></div>');
 
+  // define current article div in article-list
   const articleDiv = $('.article-list .article').get(i);
 
-  $(articleDiv).append('<img class="thumbnail" src="' + getThumbnail(articleData) + '"/>');
-  $(articleDiv).append('<h2 class="article-title"><a href="' + articleData.web_url + '" target="_blank">' + articleData.headline.main + '</a></h2>');
-  $(articleDiv).append('<p class="lead-para">' + articleData.lead_paragraph + '</p>');
-  $(articleDiv).append('<p class="byline"><span class="author">' + articleData.byline.original + '</span><span class="timestamp">' + formatTimestamp(articleData.pub_date) + '</span></p>');
+  // add HTML elements for article
+  $(articleDiv).append(
+    '<img class="thumbnail" src="' +
+    getThumbnail(articleData) +
+    '"/>'
+  );
+
+  $(articleDiv).append('<div class="wrapper"></div>');
+
+  $(articleDiv).find('.wrapper').append(
+    '<h2 class="title"><a href="' +
+    articleData.web_url +
+    '" target="_blank">' +
+    articleData.headline.main +
+    '</a></h2>'
+  );
+
+  $(articleDiv).find('.wrapper').append(
+    '<p class="lead-para">' +
+    articleData.lead_paragraph +
+    '</p>'
+  );
+
+  if (articleData.byline.original) {
+    $(articleDiv).find('.wrapper').append(
+      '<p class="byline"><span class="author">' +
+      articleData.byline.original.toLowerCase() +
+      '</span> <span class="timestamp">' +
+      formatTimestamp(articleData.pub_date) +
+      '</span></p>'
+    );
+  } else {
+    $(articleDiv).find('.wrapper').append(
+      '<p class="byline"><span class="author">By ' +
+      articleData.source.toLowerCase() +
+      '</span> <span class="timestamp">' +
+      formatTimestamp(articleData.pub_date) +
+      '</span></p>'
+    );
+  }
+
 }
 
 // return url to thumbnail if there is one or else return default placeholder
 function getThumbnail (articleData) {
   if (articleData.multimedia.length > 0) {
     const thumbnailObj = articleData.multimedia.filter(obj => {
-      return obj.subtype === "wide";
+      return obj.subtype === "xlarge";
     });
     return `http://www.nytimes.com/${thumbnailObj[0].url}`;
   } else {
@@ -93,8 +191,12 @@ function getThumbnail (articleData) {
 function formatTimestamp(dateString) {
   const date = new Date(dateString);
   const options = {
-    year: "numeric", month: "long",
-    day: "numeric", hour: "2-digit", minute: "2-digit", timeZoneName: "short"
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZoneName: "short"
   };
 
   return date.toLocaleDateString("en-us", options);
